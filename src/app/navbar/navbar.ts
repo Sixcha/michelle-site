a2c46b171f401053e1c97a1c6f7abf5682edbcc6
sixcha <paulbora_akcan@hotmail.com>
2026-05-03 23:19:41 +0200

    Start creating components

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mich-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {}
